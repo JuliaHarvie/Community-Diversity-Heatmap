a3933cafec998dc1bd37940a428278bbffc77d50
Julia H <julia.harvie1212@gmail.com>
2019-07-05 10:35:41 -0400

    Currently active script. All updates being made to here.

---
+++ Version_7.R
@@ -0,0 +1,367 @@
+#######################################################################################
+# 
+# For this run through using the case study AGAW
+#
+# 
+#====================================================================================== 
+## Function 1 - Setup
+#======================================================================================
+#
+# The first step no matter what, both boarder and trap coordinates should be uploaded in decimal degrees as either a data frame or matrix (Lat, Long), 
+# if no value is given for number of weeks will default as one. The order the trap coordinates are inputed should correspond to the order the data is 
+# in regarding traps. Ie. The first pair of coordinates in the list of trap coordiates should corespond to the first trap whos results are reported in 
+# the data sheet.If Slam trap is not set to True will assume deafutl malasis trap was used.  
+
+Setup <- function(Boarder_Coordinates, Trap_Coordinates,  Number_of_Sampling_Events = NULL, Slam_trap = FALSE){
+  ### Produces a warning if BC can not be read otherwises turns them into a vector ###
+  if(!is.data.frame(Boarder_Coordinates) && !is.matrix(Boarder_Coordinates)){
+    stop("Boarder Coordinates need to be stored as either a dataframe or a matrix.", call.= F)
+  } else {
+    BC <- c(Boarder_Coordinates[1,1],Boarder_Coordinates[1,2]) 
+    for (n in 2:nrow(Boarder_Coordinates)) {
+      BC <- c(BC,Boarder_Coordinates[n,1],Boarder_Coordinates[n,2])
+    }
+  }
+  ### Ditto for TC ###  
+  if(!is.data.frame(Trap_Coordinates) && !is.matrix(Trap_Coordinates)){
+    stop("Trap Coordinates need to be stored as either a dataframe or a matrix.", call. = F)
+  } else {
+    TC <- c(Trap_Coordinates[1,1],Trap_Coordinates[1,2]) 
+    for (n in 2:nrow(Trap_Coordinates)){
+      TC <- c(TC,Trap_Coordinates[n,1],Trap_Coordinates[n,2]) 
+    }
+  }
+  ### Calculate Number of traps ###  
+  Number_of_Traps <- length(TC)/2
+  ### if number of sampling events is set as one or left blank then 1 is assigned to the variable ###  
+  if (is.null(Number_of_Sampling_Events)||Number_of_Sampling_Events <= 0){
+    Number_of_Sampling_Events <- 1
+  }
+  storage <- list(BC, TC, Number_of_Sampling_Events, Number_of_Traps, Slam_trap)
+  return(storage)
+}
+
+# Save the results from the function "setup" to an object
+setup <- Setup(Boarder_Coordinates = read.csv(Field_Coors), Trap_Coordinates = read.csv(Trap_Coors), Number_of_Sampling_Events = 9, Slam_trap = T)
+
+#====================================================================================== 
+## Function 2 - Formatting from mBrave
+#======================================================================================
+#
+# This function is for use speficlly with TSV outputs produced by mBRrave. It returns a data frame where each column contains all 
+# the speciemen found in a single trap. Left to right the traps should be in the order their coordients were uploaded in during set up.
+# If it is a Slam trap and therefor multiple bottles associated with a singular trap. The colunms should be in the order of, north bottle,
+# east bottle, south bottle, west bottle, then repeat for the next trap. If there are multiple sampling events, repeat this layout 
+# chronolocially for each event. 
+#
+# Set ID as a character vector that represnts the repeated portion of the labeling scheme for sample location. This string should immedeatly be
+# followed by trap #,-,event #,direction (if applicable), (ex. if ID = AGAS, the location tag associated with a sample may be "AGAS4-05E") 
+# Suffix symbol identifys the text symbol (ie. ',' '_' ')' '#' etc.) that appears directly after the location tag, seperating it from 
+# additional compenents of the runName
+# Ex.If a file contains the follow as runNames "L#17AGAS1-14W)CCDB-S5-0099)CBGMB-00047", "L#17AGAS1-14S)CCDB-S5-0099)CBGMB-00047", 
+# "L#17AGAS4-05E)CCDB-S5-0077)CBGMB-00034" ID would be set as "AGAS" and Suffix Symbol as ")". 
+
+Formatting_mBrave <- function(File, ID, Suffix_Symbol, Slam_trap = F) {
+  if (!("runName" %in% names(File) && "bin_uri" %in% names (File))){
+    stop("The correct fields have not been downloaded from mBrave", call. =  F)
+  }
+  split <- strsplit(File$runName, "[)]")
+  g <- which(grepl(ID,split[[1]]))
+  seperated <- as.data.frame(matrix(0,1,4))
+  for (n in 1:length(split)){
+    split[[n]] <- str_sub(split[[n]][g], str_locate(split[[n]],ID)[1,2] + 1)
+    seperated[n,1] <- str_sub(split[[n]],str_locate_all(split[[n]], "[0-9]+")[[1]][,1],str_locate_all(split[[n]], "[0-9]+")[[1]][,2])[1]
+    seperated[n,2] <- str_sub(split[[n]],str_locate_all(split[[n]], "[0-9]+")[[1]][,1],str_locate_all(split[[n]], "[0-9]+")[[1]][,2])[2]
+    if(n == round(length(split)*0.25,0)){show("25% data pulled.")}
+    if(n == round(length(split)*0.5,0)){show("50% data pulled.")}
+    if(n == round(length(split)*0.75,0)){show("75% data pulled.")}
+    if(n == length(split)){show("100% data pulled.")}
+    if(Slam_trap) {
+      if (length(str_sub(split[[n]], str_locate_all(split[[n]], "[A-Z]+")[[1]][,1], str_locate_all(split[[n]], "[A-Z]+")[[1]][,2])) == 1){
+        if(str_sub(split[[n]], str_locate_all(split[[n]], "[A-Z]+")[[1]][,1], str_locate_all(split[[n]], "[A-Z]+")[[1]][,2]) == "N"){
+          seperated[n,3] <- 1
+        } else if(str_sub(split[[n]], str_locate_all(split[[n]], "[A-Z]+")[[1]][,1], str_locate_all(split[[n]], "[A-Z]+")[[1]][,2]) == "E"){
+          seperated[n,3] <- 2
+        } else if(str_sub(split[[n]], str_locate_all(split[[n]], "[A-Z]+")[[1]][,1], str_locate_all(split[[n]], "[A-Z]+")[[1]][,2]) == "S"){
+          seperated[n,3] <- 3
+        } else if(str_sub(split[[n]], str_locate_all(split[[n]], "[A-Z]+")[[1]][,1], str_locate_all(split[[n]], "[A-Z]+")[[1]][,2]) == "W"){
+          seperated[n,3] <- 4
+        }         
+        seperated[n,4] <- File$bin_uri[n]
+      } else if (length(str_sub(split[[n]], str_locate_all(split[[n]], "[A-Z]+")[[1]][,1], str_locate_all(split[[n]], "[A-Z]+")[[1]][,2])) >= 2) {
+        if(grep("N", str_sub(split[[n]], str_locate_all(split[[n]], "[A-Z]+")[[1]][,1], str_locate_all(split[[n]], "[A-Z]+")[[1]][,2])) == 1 ){
+          seperated[n,3] <- 1
+        } else if(grep("A",str_sub(split[[n]], str_locate_all(split[[n]], "[A-Z]+")[[1]][,1], str_locate_all(split[[n]], "[A-Z]+")[[1]][,2])) == 1){
+          seperated[n,3] <- 2
+        } else if(grep("U",str_sub(split[[n]], str_locate_all(split[[n]], "[A-Z]+")[[1]][,1], str_locate_all(split[[n]], "[A-Z]+")[[1]][,2])) == 1){
+          seperated[n,3] <- 3
+        } else if(grep("W", str_sub(split[[n]], str_locate_all(split[[n]], "[A-Z]+")[[1]][,1], str_locate_all(split[[n]], "[A-Z]+")[[1]][,2])) == 1){
+          seperated[n,3] <- 4
+        }           
+        seperated[n,4] <- File$bin_uri[n]
+      } else if (length(str_sub(split[[n]], str_locate_all(split[[n]], "[a-z]+")[[1]][,1], str_locate_all(split[[n]], "[a-z]+")[[1]][,2])) == 1){
+        if(str_sub(split[[n]], str_locate_all(split[[n]], "[a-z]+")[[1]][,1], str_locate_all(split[[n]], "[a-z]+")[[1]][,2]) == "n"){
+          seperated[n,3] <- 1
+        } else if(str_sub(split[[n]], str_locate_all(split[[n]], "[a-z]+")[[1]][,1], str_locate_all(split[[n]], "[a-z]+")[[1]][,2]) == "e"){
+          seperated[n,3] <- 2
+        } else if(str_sub(split[[n]], str_locate_all(split[[n]], "[a-z]+")[[1]][,1], str_locate_all(split[[n]], "[a-z]+")[[1]][,2]) == "s"){
+          seperated[n,3] <- 3
+        } else if(str_sub(split[[n]], str_locate_all(split[[n]], "[a-z]+")[[1]][,1], str_locate_all(split[[n]], "[a-z]+")[[1]][,2]) == "w"){
+          seperated[n,3] <- 4
+        }         
+        seperated[n,4] <- File$bin_uri        
+      } else if (length(str_sub(split[[n]], str_locate_all(split[[n]], "[a-z]+")[[1]][,1], str_locate_all(split[[n]], "[a-z]+")[[1]][,2])) >= 2){
+        if(grep("n", str_sub(split[[n]], str_locate_all(split[[n]], "[a-z]+")[[1]][,1], str_locate_all(split[[n]], "[a-z]+")[[1]][,2])) == 1 ){
+          seperated[n,3] <- 1
+        } else if(grep("a",str_sub(split[[n]], str_locate_all(split[[n]], "[a-z]+")[[1]][,1], str_locate_all(split[[n]], "[a-z]+")[[1]][,2])) == 1){
+          seperated[n,3] <- 2
+        } else if(grep("U",str_sub(split[[n]], str_locate_all(split[[n]], "[a-z]+")[[1]][,1], str_locate_all(split[[n]], "[a-z]+")[[1]][,2])) == 1){
+          seperated[n,3] <- 3
+        } else if(grep("W", str_sub(split[[n]], str_locate_all(split[[n]], "[a-z]+")[[1]][,1], str_locate_all(split[[n]], "[a-z]+")[[1]][,2])) == 1){
+          seperated[n,3] <- 4
+        }           
+        seperated[n,4] <- File$bin_uri[n]        
+      } else{
+        stop("Bottle location on SLAM Trap has not been doccumented correctly. Please use E, East, e or east in the run$Name, directly after number related to sampling event ", call. =  F)
+      }
+    } else {
+      seperated[n,3] <- File$bin_uri[n]
+    }
+  }
+  event_lables <- as.numeric(sort(unique(seperated[,2])))
+  trap_lables <- as.numeric(sort(unique(seperated[,1])))
+  ready <- data.frame()
+  if(Slam_trap){
+    ready <- as.data.frame(matrix(0,1,length(event_lables)*length(trap_lables)*4))
+    for(n in 1:length(split)){
+      col <- (which(as.numeric(seperated[n,2]) == event_lables)-1)*length(trap_lables)*4 + (which(as.numeric(seperated[n,1]) == trap_lables)-1)*4 + seperated[n,3]
+      ready[colSums(!is.na(ready))[col] + 1,col] <- seperated[n,4] 
+      if(n == 1){ready <- ready[-1,]}
+      if(n == round(length(split)*0.25,0)){show("25% formatted.")}
+      if(n == round(length(split)*0.5,0)){show("50% formatted.")}
+      if(n == round(length(split)*0.75,0)){show("75% formatted.")}
+      if(n == length(split)){show("100% formatted")}
+    }      
+  } else {
+    ready <- as.data.frame(matrix(0,1,length(event_lables)*length(trap_lables)))
+    for(n in 1:length(split)){
+      col <- (which(as.numeric(seperated[n,2]) == event_lables)-1)*length(trap_lables) + which(as.numeric(seperated[n,1]) == trap_lables)
+      ready[colSums(!is.na(ready))[col] + 1,col] <- seperated[n,3] 
+      if(n == 1){ready <- ready[-1,]}
+      if(n == round(length(split)*0.25,0)){show("25% formatted.")}
+      if(n == round(length(split)*0.5,0)){show("50% formatted.")}
+      if(n == round(length(split)*0.75,0)){show("75% formatted.")}
+      if(n == length(split)){show("100% formatted")}
+    }
+  }
+  return(ready)  
+}
+
+# A data frame has been created that has the correct layout to run thrught the rest of the code
+formatted <- Formatting_mBrave(File=File,ID = "AGAS", Suffix_Symbol = ")", Slam_trap = T)
+
+#====================================================================================== 
+## Function 3 - Data
+#======================================================================================
+#
+# The next step is to read in the sampling data and make sure it matches with paramters provided in set up. Use the function data to create the boject that holds 
+#all sample information in a format all further functions will use.
+
+Data <- function(File, Setup) {
+  if (!is.data.frame(File)){
+    stop("Sampling data must be stored as a dataframe.", call. = F)
+  }
+  if (Setup[[5]] && ncol(File) != (Setup[[4]]*4*Setup[[3]])){
+    stop("Data provided for file does not match the parameters used in setup", calli.=F)
+  } else if (!(Setup[[5]]) && ncol(File) != (Setup[[4]]*Setup[[3]])) {
+    stop("Data provided for file does not match the parameters used in setup", calli.=F)
+  }  
+  trap <- list(c(1))
+  for(n in 1:ncol(File)){
+    trap[[(n)]] <- (File[,n])
+    trap[[(n)]] <- as.character(trap[[(n)]])
+    trap[[(n)]] <- trap[[(n)]][trap[[(n)]]!=""]
+  }
+  return(trap)
+}
+
+# Include the object created from function 1 (setup) and a dataframe containing all of the samples 
+data <- Data(File = read.csv(CSV), Setup = setup)
+
+#====================================================================================== 
+## Function 4 - Distance Conversions
+#======================================================================================
+#
+# The function will be used to convert sets of decimal degrees coordinates into a distance vector in future calculations.
+# The distance vectors will be in metres unless km is set to true, then switches to kilometres. 
+
+Distance <- function(y1,y2,x1,x2, km = F){
+  varR <- 6371e3
+  radians <- function(d) {
+    d * pi / 180
+  }
+  lat1 <- radians(y1)
+  lat2 <- radians(y2)
+  latdif <- radians((y2 - y1))
+  londif <- radians((x2-x1))
+  equ1 <- (sin((latdif/2)))^2 + cos(lat1) * cos(lat2) * (sin((londif/2)))^2
+  equ2 <- 2 * atan2(sqrt(equ1),sqrt(1-equ1))
+  if (!(km)){
+    return(varR*equ2)
+  } else {
+    return(varR*equ2/1000)
+  }
+}
+
+# There is no object that intial needs to be created. The function must be read in so that it can be used in additional functions.
+
+#====================================================================================== 
+## Function 5 - Conversion
+#======================================================================================
+#
+# Convert coordinates into (X,Y) points that can be plotted onto a 2D graph therefor assigning 0,0 as the south west corner.
+
+Conversion <- function(Setup, Poles = F){
+  ### Seperating the corners on the plot ###
+  k <- 2
+  xcorners <- c()
+  for (x in 1:(length(Setup[[1]])/2)){
+    xcorners[x] <- Setup[[1]][k]
+    k <- k+2
+  }
+  k <- 1
+  ycorners <- c()
+  for (y in 1:(length(Setup[[1]])/2)){
+    ycorners[y] <- Setup[[1]][k]
+    k <- k+2
+  }
+  ### Pulling out crtical points form these corners ###
+  ### Need to define the area we will lay the array over to perform calculations. Will use terms "top, bottom, left, right". ###
+  ### Almost always these will be synonymous with "north, south, west, east" respectively, but not always so will keep it simple with 2D terms. ###    
+  if(Poles) {
+    if(ycorners[1] > 0){
+      ### Calculates length of x axis (starts at LMX goes to RMX along the line of BMY) ###
+      LMX <- 180
+      BMY <- min(ycorners)
+      DX <- round(Distance(BMY,BMY,180,0),0)
+      ### Calculates length of y axis (starts at BMY goes to TMY along the line of LMX) ###     
+      DY <- DX
+    } else {
+      LMX <- 180
+      BMY <- max(ycorners)     
+      DX <- round(Distance(BMY,BMY,180,0),0)
+      DY <- DX
+    } 
+    ### Using this solution to solve for the poles is computationally intensive but because the liklihood of this senerio happening is low. Not worth 
+    ### finding a better solution. ###
+  } else {
+    ### Calculates length of x axis (starts at LMX goes to RMX along the line of BMY) ###
+    BMY <- min(ycorners)
+    LMX <- min(xcorners)
+    DX <- round(Distance(min(ycorners),min(ycorners),min(xcorners),max(xcorners)),0)
+    ### Calculates length of y axis (starts at BMY goes to TMY along the line of LMX) ###
+    DY <- round(Distance(min(ycorners),max(ycorners),min(xcorners),min(xcorners)),0)
+  }
+  ### Now need to convert other corners into distances relative to the bottom left corner of the newly defined plot  
+  plot_outline <- c()
+  for (k in 1:length(xcorners)){
+    plot_outline <- c(plot_outline, round(Distance(BMY,BMY,LMX,xcorners[k]),0),round(Distance(BMY,ycorners[k],LMX,LMX),0))
+  }
+  k <- 2
+  xcorners <- c()
+  for (x in 1:(length(Setup[[2]])/2)){
+    xcorners[x] <- Setup[[2]][k]
+    k <- k+2
+  }
+  k <- 1
+  ycorners <- c()
+  for (y in 1:(length(Setup[[2]])/2)){
+    ycorners[y] <- Setup[[2]][k]
+    k <- k+2
+  }
+  trap_locations <- c()
+  for (k in 1:length(xcorners)){
+    trap_locations <- c(trap_locations, round(Distance(BMY,BMY,LMX,xcorners[k]),0),round(Distance(BMY,ycorners[k],LMX,LMX),0))
+  }
+  return(list(plot_outline,trap_locations))
+}
+
+conversion <- Conversion(Setup = setup)
+
+#====================================================================================== 
+## Function 6 - Array Placment 
+#======================================================================================
+#
+# Takes the the converted quarentents and uses them to build an array as well as being to populate it with known abundance valueas. 
+
+Array_placement <- function(Setup, Outlines, Data){
+  all_matrix <- list()
+  traps <- list()
+  if (Setup[[5]]){
+    for(m in 1:Setup[[3]]){
+      all_matrix[[m]] <- matrix(0,Outlines[[2]],Outlines[[1]])
+      for (k in 1:Setup[[4]]){
+        all_matrix[[m]][Outlines[[4]][(2*k)]+1,Outlines[[4]][(2*k-1)]] <- length(Data[[((m-1)*4*Setup[[4]])+k*4-3]])
+        all_matrix[[m]][Outlines[[4]][(2*k)],Outlines[[4]][(2*k-1)]+1] <- length(Data[[((m-1)*4*Setup[[4]])+k*4-2]])
+        all_matrix[[m]][Outlines[[4]][(2*k)]-1,Outlines[[4]][(2*k-1)]] <- length(Data[[((m-1)*4*Setup[[4]])+k*4-1]])
+        all_matrix[[m]][Outlines[[4]][(2*k)],Outlines[[4]][(2*k-1)]-1] <- length(Data[[((m-1)*4*Setup[[4]])+k*4]])
+        if(m == 1){
+          traps[[k]]<- list(c(Outlines[[4]][(2*k-1)],Outlines[[4]][(2*k)]),c(1:4)+(4*(k-1)))
+        }
+      }
+    }
+  } else {
+    for(m in 1:Setup[[3]]){
+      all_matrix[[m]] <- matrix(0,Outlines[[2]],Outlines[[1]])
+      for (k in 1:Setup[[4]]){
+        all_matrix[[m]][Outlines[[4]][(2*k)]+1,Outlines[[4]][(2*k-1)]] <- length(Data[[((m-1)*Setup[[4]])+k]])
+        all_matrix[[m]][Outlines[[4]][(2*k)],Outlines[[4]][(2*k-1)]+1] <- length(Data[[((m-1)*Setup[[4]])+k]])
+        all_matrix[[m]][Outlines[[4]][(2*k)]-1,Outlines[[4]][(2*k-1)]] <- length(Data[[((m-1)*Setup[[4]])+k]])
+        all_matrix[[m]][Outlines[[4]][(2*k)],Outlines[[4]][(2*k-1)]-1] <- length(Data[[((m-1)*Setup[[4]])+k]])
+        if(m == 1){
+          traps[[k]]<- list(c(Outlines[[4]][(2*k-1)],Outlines[[4]][(2*k)]),c(1:4)+(4*(k-1)))
+        }
+      }
+    }
+  }
+  sort_y <- c()  
+  for(T in 1:Setup[[4]]){
+    sort_y[T] <- traps[[T]][[2]]  
+  }
+  ### top to bottom (NtoS) ###  
+  TtoB <- sort(sort_y,decreasing =TRUE)
+  TtoBmatrix <- matrix(NA, Setup[[4]]+1, 5)
+  for (r in 1:5){
+    for (k in 1:5){
+      if (TtoB[r] == traps[[k]][[2]]){
+        TtoBmatrix[r,] <- c(traps[[k]][[3]], NA)
+      }
+    }
+  }
+  sort_x <- c()  
+  for(T in 1:Setup[[4]]){
+    sort_x[T] <- traps[[T]][[1]]  
+  }
+  ### right to left (east to west) ###  
+  RtoL <- sort(sort_x,decreasing =TRUE)  
+  RtoLmatrix <- matrix(NA, Setup[[4]]+1, 5)
+  for (r in 1:Setup[[4]]){
+    for (k in 1:Setup[[4]]){
+      if (RtoL[r] == traps[[k]][[1]]){
+        RtoLmatrix[r,] <- c(traps[[k]][[3]], NA)
+      }
+    }
+  }
+  return(list(all_matrix,traps,TtoBmatrix,RtoLmatrix))
+}
+
+
+# The object returned from this function is a two part list. For simplicity it is recommened to segrate them into their own objects. As done below
+plots <- Array_placement(Setup = setup, Outlines = conversion, Data = data)[[1]]
+traps <- Array_placement(Setup = setup, Outlines = conversion, Data = data)[[2]]
+# I am going to name the object containing my direction matrices using north, south, east and west, not top, bottom, right and left because
+# conceptually easier, but note techincally is still refereing to positions in an array which does not have geographical directions  
+NtoSmatrix <- Array_placement(Setup = setup, Outlines = conversion, Data = data)[[3]] 
+EtoWmatrix <- Array_placement(Setup = setup, Outlines = conversion, Data = data)[[4]] 
+
+
